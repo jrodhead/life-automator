85cd9c65770a2bb9bcde363b57a0ad6058d5480e
jrodhead <13279619+jrodhead@users.noreply.github.com>
2023-07-06 15:20:01 -0500

    add familyRoadTrip story

---
+++ wander-words/familyRoadTrip.js
@@ -0,0 +1,11 @@
+let story = `Once upon a time, a ${adjective1} family embarked on an exciting road trip adventure. The journey began in their trusty ${noun1}, heading towards the enchanting destination of ${noun2}. Along for the ride were ${person1}, ${person2}, ${person3}, and ${person4}, filled with anticipation and joy.
+
+As they drove through picturesque landscapes, they encountered ${adjective2} landmarks, such as the majestic ${noun3} and the breathtaking ${noun4}. Laughter filled the air as they played ${verb1} games and sang ${verb2} songs together.
+
+Throughout their expedition, the family encountered friendly locals who shared fascinating stories and recommended delicious ${noun5} at local diners. They also stumbled upon hidden ${noun6} and ${noun7} that sparked their curiosity and sense of adventure.
+
+As the sun began to set, they found the perfect spot to set up camp near a tranquil ${noun8}. The family gathered around a cozy campfire, roasting marshmallows and sharing tales of their day's escapades.
+
+With hearts full of memories, they continued their road trip, creating lasting bonds and experiencing the joy of exploring new places as a loving family.
+
+And so, the adventure of this extraordinary family on their road trip journey will be cherished forever, as they look forward to more exciting wanderings in the future.`;
---
+++ wander-words/howToStoryPrompt.md
@@ -0,0 +1,10 @@
+# Prompt ChatGPT for a new story
+
+Using the conversation that already contains context for WanderWords, submit the following:
+
+```
+create a new WanderWords story
+- theme or setting:
+- description of the main characters or elements in the story:
+- total number of variables (e.g., adjectives, nouns, verbs, names):
+```
