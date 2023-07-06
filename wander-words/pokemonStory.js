08ab8bae72f13096c8de447b4af5342fd4847745
jrodhead <13279619+jrodhead@users.noreply.github.com>
2023-07-06 14:41:35 -0500

    add pokemon story

---
+++ wander-words/pokemonStory.js
@@ -0,0 +1,11 @@
+let story = `Once upon a time, in the world of Pokemon, there were two ${adjective1} children named ${person1} and ${person2}. One day, they stumbled upon a mysterious ${noun1} and a magical ${noun2}. Excitedly, they ${verb1} and ${verb2} with their new discoveries.
+
+Little did they know, their adventures caught the attention of legendary Pokemon ${pokemon1} and ${pokemon2}. These powerful creatures appeared before ${person1} and ${person2}, ready to embark on thrilling journeys together.
+
+With their trusty Pokemon companions, ${person1} and ${person2} ventured through vast lands, battling fierce opponents and collecting valuable items. Their journey was filled with ${adjective2} challenges and unforgettable encounters.
+
+As time went on, ${person1} and ${person2} became renowned Pokemon trainers, known for their extraordinary skills and unwavering determination. They inspired others with their bravery and compassion.
+
+Their story became a legend in the Pokemon world, passed down through generations. It showcased the incredible bond between humans and Pokemon, teaching everyone the importance of friendship and teamwork.
+
+And so, the adventures of ${person1}, ${person2}, and their beloved Pokemon continue, creating new tales and leaving a lasting impact on the world of Pokemon.`;
